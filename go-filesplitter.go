fce134a3160ca3515207092cf80b19acae93236e
TANAKA Shunichi <tanaka-shu@gnavi.co.jp>
2020-04-18 11:08:46 +0900

    [update]ファイル格納先修正

--- go-filesplitter.go
+++ go-filesplitter.go
@@ -4,37 +4,40 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.POST("/api/fileupload", func(c echo.Context) error {
-
-		file, err := c.FormFile("file")
-		if err != nil {
-			return err
-		}
-
-		src, err := file.Open()
-		if err != nil {
-			return err
-		}
-		defer src.Close()
-
-		dst, err := os.Create("file.Filename")
-		if err != nil {
-			return err
-		}
-		defer dst.Close()
-
-		if _, err = io.Copy(dst, src); err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, "file has uploaded")
-	})
-
+	e.POST("/api/fileupload", fileupload)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func fileupload(c echo.Context) error {
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	filename := filepath.Join("tmp", file.Filename)
+	dst, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, "file has uploaded")
 }
