62a33079caac315e8f5b8fd40e78819c05c5c9c0
TANAKA Shunichi <nekodanshaku27@gmail.com>
2020-05-05 16:18:35 +0900

    [update]シングルバイナリ対応

--- route/route.go
+++ route/route.go
@@ -1,28 +1,31 @@
 package route
 
 import (
-	"github.com/tana-dev/go-filesplitter/handler"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rakyll/statik/fs"
+	"github.com/tana-dev/go-filesplitter/api"
+	_ "github.com/tana-dev/go-filesplitter/statik"
 )
 
 func Init() *echo.Echo {
 
 	e := echo.New()
 
-	// Set Bundle MiddleWare
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORS())
-	//	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	//		AllowOrigins: []string{"*"},
-	//		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAcceptEncoding},
-	//	}))
+
+	// statik
+	statikFS, _ := fs.New()
+	h := http.FileServer(statikFS)
+	e.GET("/*", echo.WrapHandler(http.StripPrefix("/", h)))
 
 	// Routes
-	v1 := e.Group("/api/v1")
+	apiGroup := e.Group("/api/v1")
 	{
-		v1.POST("/filesplit", handler.PostFilesplit)
+		apiGroup.POST("/filesplit", api.PostFilesplit)
 	}
 
 	return e
